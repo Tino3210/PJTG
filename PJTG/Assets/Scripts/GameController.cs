7ec8667a861b8c741cbce8375ef0e37724291f3f
Bruno Costa <bruno.costaalexandre>
2022-11-19 00:07:51 +0100

    WIP : Wave management

--- PJTG/Assets/Scripts/GameController.cs
+++ PJTG/Assets/Scripts/GameController.cs
@@ -27,12 +27,14 @@ public class GameController : MonoBehaviour {
         }
     }
 
+    // Display the timer as minutes and seconds
     private void DisplayTime(float timeToDisplay) {
         float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    // Add the score on the user's score
     public void AddScore(int score) {
         this.score += score;
         scoreText.text = "Score : " + this.score;
---
+++ PJTG/Assets/Scripts/Wave/EnemyType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType {
+    BURGER = 10,
+    FRIES = 20,
+    SANDWICH = 30
+}
